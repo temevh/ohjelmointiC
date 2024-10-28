c9a905cb8f6d8e3fc64f299509a7074f2d2ddb7d
Teemu H <temehama@gmail.com>
2024-10-28 16:23:11 +0200

    I hate C standards

--- osa_3/fibonacci.c
+++ osa_3/fibonacci.c
@@ -4,35 +4,32 @@
 
 int main(void)
 {
-  //unsigned int *fibonacci = fib_jono(15);
-  fib_jono(15);
-  /*
-
   int i = 0;
+  unsigned int *fibonacci = fib_jono(15);
+  fib_jono(15);
+  
+
   for(i = 0; i < 15; ++i)
   {
     printf(" %u", fibonacci[i]);
   }
   printf("\n");
   free(fibonacci);
-  */
+  
   return 0;
 }
 
 
-void fib_jono(int n){
-  int f0 = 0;
-  int f1 = 1;
-  int next = f0 + f1;
+unsigned int * fib_jono(unsigned int n){
+  int i = 2;
 
-  printf("%d %d ", f0, f1);
+  unsigned int *pArray = malloc(sizeof(unsigned int) * n);
+  pArray[0] = 0;
+  pArray[1] = 1;
 
-  for(int i = 3; i <= n; ++i){
-    printf("%d ", next);
-    f0 = f1;
-    f1 = next;
-    next = f0 + f1;
+   for (; i < n; ++i) {
+    pArray[i] = pArray[i - 1] + pArray[i - 2];
   }
-  printf("\n");
 
+  return pArray;
 }
--- osa_3/fibonacci.h
+++ osa_3/fibonacci.h
@@ -1,7 +1,6 @@
 #ifndef FIBONACCI_H
 #define FIBONACCI_H
 
-//unsigned int * fib_jono(unsigned int n);
-void fib_jono(int n);
+unsigned int * fib_jono(unsigned int n);
 
 #endif
