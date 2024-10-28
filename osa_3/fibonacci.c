405fcb520f4b4c1c86e7e5a56a44340ab41a9582
Teemu H <temehama@gmail.com>
2024-10-28 16:03:33 +0200

    fibo no malloc

--- osa_3/fibonacci.c
+++ osa_3/fibonacci.c
@@ -4,7 +4,10 @@
 
 int main(void)
 {
-  unsigned int *fibonacci = fib_jono(15);
+  //unsigned int *fibonacci = fib_jono(15);
+  fib_jono(15);
+  /*
+
   int i = 0;
   for(i = 0; i < 15; ++i)
   {
@@ -12,5 +15,24 @@ int main(void)
   }
   printf("\n");
   free(fibonacci);
+  */
   return 0;
+}
+
+
+void fib_jono(int n){
+  int f0 = 0;
+  int f1 = 1;
+  int next = f0 + f1;
+
+  printf("%d %d ", f0, f1);
+
+  for(int i = 3; i <= n; ++i){
+    printf("%d ", next);
+    f0 = f1;
+    f1 = next;
+    next = f0 + f1;
+  }
+  printf("\n");
+
 }
---
+++ osa_3/fibonacci.h
@@ -0,0 +1,7 @@
+#ifndef FIBONACCI_H
+#define FIBONACCI_H
+
+//unsigned int * fib_jono(unsigned int n);
+void fib_jono(int n);
+
+#endif
